dc0c5819508ffcb8d0a6736c121bc7d9458ce2c2
MiguelAngelRoldan <MiguelAngel.roldan1@alu.uclm.es>
2022-12-27 20:05:30 +0100

    Todos los docker creados

---
+++ demo-ssh/docker/broker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ip route add 10.0.2.0/24 via 10.0.1.2
+
+echo "10.0.2.3    auth" >> /etc/hosts
+echo "10.0.2.4    files" >> /etc/hosts
+
+
+if [ -z "$@" ]; then
+    exec /bin/bash
+else
+    exec $@
+fi
